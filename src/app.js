71b740f59a1ec1ccc12e9baf8453fc69833762e4
RojanShrestha1992 <pheonixeder@gmail.com>
2026-02-15 15:29:44 +0545

    feat: implement account creation and authentication middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const cooikeParser = require('cookie-parser')
+
+/**
+ * Routes
+ */
 const authRouter = require('./routes/auth.routes')
+const accountRouter = require('./routes/account.routes')
 
 const app = express()
-
-
 app.use(express.json())
 app.use(cooikeParser())
+
+
+/**
+ * use routers
+ */
 app.use('/api/auth', authRouter)
+app.use('/api/accounts', accountRouter)
 
 
 module.exports = app
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,29 +1,38 @@
 const mongoose = require("mongoose");
 
-const accountSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-        required: [true, "User is required for creating an account"]
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "User is required for creating an account"],
+      index: true,
     },
-    status:{
-        enum: {
-            values: ["ACTIVE","FROZEN", "CLOSED" ],
-            message: "Status must be either ACTIVE, FROZEN, or CLOSED"
-        }
-    },
-    currency:{
+    status: {
         type: String,
-        required: [true, "Currency is required for creating an account"],
-        default: "USD",
-        match: [/^[A-Z]{3}$/, "Currency must be a valid ISO 4217 code (3 uppercase letters)"]
-    }
-},{
-    timestamps: true
-})
+      enum: {
+        values: ["ACTIVE", "FROZEN", "CLOSED"],
+        message: "Status must be either ACTIVE, FROZEN, or CLOSED",
+    },
+    default: "ACTIVE",
+    },
+    currency: {
+      type: String,
+      required: [true, "Currency is required for creating an account"],
+      default: "USD",
+      match: [
+        /^[A-Z]{3}$/,
+        "Currency must be a valid ISO 4217 code (3 uppercase letters)",
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
+accountSchema.index({ user: 1, status: 1 });
 
+const accountModel = mongoose.model("account", accountSchema);
 
-const accountModel = mongoose.model("account", accountSchema)
-
-module.exports = accountModel
+module.exports = accountModel;
